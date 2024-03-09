25d7b22ebf12a878a9836667ad22f6ec6eddf7b6
ashokv54 <ashokvdeveloper@gmail.com>
2024-03-09 09:48:09 +0000

    added sql project

---
+++ src/database/dbo/Tables/TodoItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TodoItems] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NOT NULL,
+    [IsComplete] INT            NOT NULL,
+    CONSTRAINT [PK_TodoItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
